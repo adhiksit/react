95a85c4f2fe41d4c52212ec289d9f081be584c92
adhiksit <adhiksitadi@gmail.com>
2026-03-22 00:14:25 +0530

    okay

---
+++ 04_mini_project(props)/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <div className="top">
+          <img src="" alt="" />
+          <button>Save</button>
+        </div>
+        <div className="center">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
