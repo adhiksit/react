5e293c41a22a06a9fe579fc90c57052ecbd45633
adhiksit <adhiksitadi@gmail.com>
2026-04-11 03:13:07 +0530

    properties

--- 03_props/src/components(inside)/Card.jsx
+++ 03_props/src/components(inside)/Card.jsx
@@ -4,10 +4,10 @@ const Card = (props) => {
     console.log(props.user)
   return (
     <div className="card">
-            <h1>{props.user} {props.age}</h1>
-            <img src = {props.img}/>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
-            <button>Click me</button>
+      <h1>{props.user} {props.age}</h1>
+      <img src = {props.img}/>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+      <button>Click me</button>
     </div>
   )
 }
