969457477b5b0aff8e6b73f5720999046a2138f9
adhiksit <adhiksitadi@gmail.com>
2026-04-11 21:02:42 +0530

    very basic of tailwind

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-500'>
+      <h1>
+        Hello
+      </h1>
+    </div>
+  )
+}
+
+export default App
